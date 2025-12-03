fb67a9e7f99b90ea07fbbc38eb34fae63e2993d0
Xinyi Li <wolixinyi@gmail.com>
2025-12-03 01:40:10 -0800

    day 1 py

---
+++ src/day01.py
@@ -0,0 +1,50 @@
+import sys
+input = sys.stdin.readline
+
+def read_input(path: str) -> list[str]:
+    with open(path, 'r') as file:
+        return [line.strip() for line in file.readlines()]
+
+
+def solve1(lst):
+    cur = 50
+    res = 0
+    for line in lst:
+        direction = line[0]
+        value = int(line[1:])
+        if direction == 'L':
+            cur -= value
+        elif direction == 'R':
+            cur += value
+        cur %= 100
+        if cur == 0:
+            res += 1
+    return res
+
+def solve2(lst):
+    cur = 50
+    res = 0
+    for line in lst:
+        direction = line[0]
+        value = int(line[1:])
+        new_cur = cur
+        if direction == 'L':
+            new_cur -= value
+        elif direction == 'R':
+            new_cur += value
+        l, r = sorted([cur, new_cur])
+        # count how many multiples of 100 are in [l, r]
+        res += (r // 100) - ((l - 1) // 100)
+        if cur % 100 == 0:
+            res -= 1
+        cur = new_cur % 100
+    return res
+
+if __name__ == "__main__":
+    input_path = '../input/day01.txt'
+    lines = read_input(input_path)
+    res1 = solve1(lines)
+    print(res1)
+    res2 = solve2(lines)
+    print(res2)
+
